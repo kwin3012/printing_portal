6c4ab1e5e584074872170ee6ebfb2f545b452f2a
Kevin Patel <kpatel@iitg.ac.in>
2022-04-04 19:43:51 +0530

    created model Order

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
--- users/models.py
+++ users/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User,AbstractBaseUser
+from django.forms import CharField, EmailField, FileField
+from django.utils import timezone
 
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+
+class Order(models.Model):
+    # user info
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    # shop keeper info
+    shopkeeper_email = models.EmailField(max_length=100)
+    shopkeeper_location = models.CharField(max_length=100)
+
+    # file info
+    file = models.FileField(default="blank.pdf",upload_to='')
+    no_of_copies = models.IntegerField(default=1)
+    black_and_white = models.BooleanField(default=True)
+
+    #order details
+    cost = models.IntegerField()
+    date_ordered = models.DateTimeField(default=timezone.now)
+
+
+
+
+
 
 
 
