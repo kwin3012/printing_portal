a02bd2d9bd91b3403922b1122df9c4e793e3fab7
Kevin Patel <kpatel@iitg.ac.in>
2022-04-07 17:37:11 +0530

    improved css

--- printing_portal/settings.py
+++ printing_portal/settings.py
@@ -132,3 +132,14 @@ MEDIA_URL = 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
